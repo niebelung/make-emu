eb3bada46af320a8af89676c216b2c6e22c398e6
Криштоф Антон <a.krishtof@ntc-epsilon.com>
2015-08-18 12:49:42 +0300

    Minor beautification

--- main.cpp
+++ main.cpp
@@ -57,7 +57,7 @@ int main(int argc, char **argv) {
             std::cout << "\"" << target.name() << "\" :" << std::endl;
             for(auto & action : target.actions())
             {
-                actionProcessor->execute(*action.get());
+                actionProcessor->execute(*action);
             }
             target.markProcessed();
             return true;
